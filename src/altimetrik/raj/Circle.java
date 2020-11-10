e32a3dc97a4ed31444dd104f4c64d03cfbca880e
Raj_Kumar <rajkumar.innocube@gmail.com>
2020-11-10 14:50:14 +0530

    Initial commit for Cylinder_InheritanceSample

---
+++ src/altimetrik/raj/Circle.java
@@ -0,0 +1,18 @@
+package altimetrik.raj;
+
+public class Circle {
+    private double radius;
+    Circle(double radius){
+        if (radius<0)
+            this.radius=0;
+        else
+        this.radius=radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return (radius*radius*Math.PI);
+    }
+}
